b37ca2b80826c7d69638a0a02c4618027ed6ce04
Naveen <naveen.g@spurtreetech.com>
2019-09-08 16:24:49 +0530

    Login pin, Transaction, Verify pin changes

--- react/complete/src/pages/SetTransactionPin/SetTransactionPin.js
+++ react/complete/src/pages/SetTransactionPin/SetTransactionPin.js
@@ -1,15 +1,83 @@
 import React from 'react';
+import TextInput from '../../components/TextInput';
 import PropTypes from 'prop-types';
 import styles from './SetTransactionPin.module.css';
 
-const SetTransactionPin = props => {
-  return (
-    <div className={styles.root}></div>
-  );
-};
+// const SetTransactionPin = props => {
+//   return (
+//     <div className={styles.root}></div>
+//   );
+// };
 
-SetTransactionPin.defaultProps = {};
+// SetTransactionPin.defaultProps = {};
 
-SetTransactionPin.propTypes = {};
+// SetTransactionPin.propTypes = {};
+
+class  SetTransactionPin extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      pin:"",
+      confirmpin:""
+    };
+    this.buttonClick = this.buttonClick.bind(this);
+    this.textChanged = this.textChanged.bind(this);
+  }
+  textChanged(event){
+    if(event !== null && event.target !== null && event.target !== undefined){
+      this.setState({
+        [event.target.name]:event.target.value
+      });
+    }
+  }
+  buttonClick(event){
+    if(event !== null && event.target !== null && event.target !== undefined){
+      if(event.target.name === "verifypin"){
+        alert("Feature Under Development");
+      }
+    }
+  }
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+            <div className="offset-md-3 col-md-5">
+                <div className="loginOut">
+                    <h4>Last step.</h4>
+                    <p>Set your transaction pin</p>
+                    <div className="form-group">
+                    <TextInput
+                          className="form-control"
+                          type="text"
+                          name="pin"
+                          id="pin"
+                          placeholder="Set Pin"
+                          value={this.state.pin}
+                          handleChange={this.textChanged}
+                      />
+                    </div>
+                    <div className="form-group">
+                    <TextInput
+                          className="form-control"
+                          type="text"
+                          name="confirmpin"
+                          id="confirmpin"
+                          placeholder="Re-enter Pin"
+                          value={this.state.confirmpin}
+                          handleChange={this.textChanged}
+                      />
+                    </div>
+                    <div className="row">
+                        <div className="offset-md-3 col-md-6 text-center">
+                            <button name="verifypin" type="button" onClick={(event)=>this.buttonClick(event)}>Make me an account</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
 
 export default SetTransactionPin;
--- react/complete/src/pages/VerifyPin/VerifyPin.js
+++ react/complete/src/pages/VerifyPin/VerifyPin.js
@@ -1,15 +1,69 @@
 import React from 'react';
+import TextInput from '../../components/TextInput';
 import PropTypes from 'prop-types';
 import styles from './VerifyPin.module.css';
 
-const VerifyPin = props => {
-  return (
-    <div className={styles.root}></div>
-  );
-};
+// const VerifyPin = props => {
+//   return (
+//     <div className={styles.root}></div>
+//   );
+// };
 
-VerifyPin.defaultProps = {};
+// VerifyPin.defaultProps = {};
 
-VerifyPin.propTypes = {};
+// VerifyPin.propTypes = {};
 
+class VerifyPin extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      verifypin:""
+    }
+    this.buttonClick = this.buttonClick.bind(this);
+    this.textChanged = this.textChanged.bind(this);
+  }
+  textChanged(event){
+    if(event !== null && event.target !== null && event.target !== undefined){
+      this.setState({
+        [event.target.name]:event.target.value
+      });
+    }
+  }
+  buttonClick(event){
+    if(event !== null && event.target !== null && event.target !== undefined){
+      if(event.target.name === "verify"){
+        alert("Feature Under Development");
+      }
+    }
+  }
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+            <div className="offset-md-3 col-md-5">
+                <div className="loginOut">
+                    <h4>Verification</h4>
+                    <p>Please enter the OTP send to your registered emial/mobile number.</p>
+                    <div className="form-group">
+                      <TextInput
+                          className="form-control"
+                          type="text"
+                          name="verifypin"
+                          id="verifypin"
+                          value={this.state.verifypin}
+                          handleChange={this.textChanged}
+                        />
+                    </div>
+                    <div className="row">
+                        <div className="offset-md-3 col-md-6 text-center">
+                            <button name="verify" type="button" onClick={(event)=>this.buttonClick(event)}>Verify</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    )
+  }
+}
 export default VerifyPin;
